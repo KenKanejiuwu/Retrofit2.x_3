683ef14d03aecfef1ee2e5a1370d215b320c33e2
KenKanejiuwu <eeparengandon@icloud.com>
2021-11-22 11:35:12 +0900

    Retrofit3

---
+++ app/src/main/java/com/example/a09112021_3/Repos.java
@@ -0,0 +1,18 @@
+package com.example.a09112021_3;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Repos {
+
+    @SerializedName("name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
